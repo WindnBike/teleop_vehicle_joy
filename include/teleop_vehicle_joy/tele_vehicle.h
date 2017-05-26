726eb2a473534ad1c2c82f4cbdc8628c11b0a0c1
WindnBike <windnbike@qq.com>
2017-05-26 14:42:18 +0800

    fist commit

---
+++ include/teleop_vehicle_joy/tele_vehicle.h
@@ -0,0 +1,16 @@
+//
+// Created by WindnBike on 2017/5/26.
+//
+
+#ifndef TELEOP_VEHICLE_JOY_TELE_VEHICLE_H
+#define TELEOP_VEHICLE_JOY_TELE_VEHICLE_H
+
+namespace teleop_vehicle_joy {
+
+    class tele_vehicle {
+
+    };
+
+}  // namespace teleop_vehicle_joy
+
+#endif //TELEOP_VEHICLE_JOY_TELE_VEHICLE_H
---
+++ src/tele_vehicle.cpp
@@ -0,0 +1,10 @@
+//
+// Created by WindnBike on 2017/5/26.
+//
+
+#include "teleop_vehicle_joy/tele_vehicle.h"
+namespace teleop_vehicle_joy {
+
+
+
+}  // namespace teleop_vehicle_joy
